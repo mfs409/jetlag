1d1d8df7859c1af313f7ed458225100eda95aff9
Michael Spear <spear@lehigh.edu>
2024-01-18 21:10:10 -0500

    BUGFIX: level music queues multiple instances when requested in splash, resulting in a cacophony; STATUS: resolved

--- src/jetlag/Components/Music.ts
+++ src/jetlag/Components/Music.ts
@@ -2,14 +2,37 @@ import { ISound } from "../Services/AudioLibrary";
 
 /** MusicComponent manages the background music for a stage */
 export class MusicComponent {
+  /** Track if the music is playing */
+  private is_playing = false;
+
   /** If the level has music attached to it, this starts playing it */
-  public play() { if (this.music && !this.music.playing()) this.music.play(); }
+  public play() {
+    if (this.music && !this.music.playing() && !this.is_playing) {
+      this.is_playing = true;
+      this.music.play();
+    }
+  }
 
   /** If the level has music attached to it, this pauses it */
-  public pause() { if (this.music && this.music.playing()) this.music.pause(); }
+  public pause() {
+    if (this.music && this.music.playing() && this.is_playing) {
+      this.is_playing = false;
+      this.music.pause();
+    }
+  }
 
   /** If the level has music attached to it, this stops it */
-  public stop() { if (this.music && this.music.playing()) this.music.stop(); }
+  public stop() {
+    if (this.music && this.music.playing() && this.is_playing) {
+      this.is_playing = false;
+      this.music.stop();
+    }
+  }
+
+  /** Report if there is music that is playing */
+  public playing() {
+    return (this.music) ? this.music.playing() && this.is_playing : false;
+  }
 
   /**
    * Construct a MusicComponent by providing a music object to play.
