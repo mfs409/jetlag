5005de19ab4382110cde50a137b76f63f30c4a6c
Michael Spear <spear@lehigh.edu>
2024-01-18 22:25:41 -0500

    Added a step to the audio tutorial to discuss accessing sound effects from a gesture.  Also discussed pause, stop, playing, and how to use the return value of play() to have fine-grained control

--- src/tutorials/assets_audio_animations.ts
+++ src/tutorials/assets_audio_animations.ts
@@ -573,6 +573,83 @@ function builder(level: number) {
     // You'll notice that long sounds don't work very well.  It's up to you to
     // create delays as needed by your soundtrack.
   }
+
+  else if (level == 16) {
+    // Let's also look at how to make sound effects in response to gestures.
+    // This idea generalizes to any reason for making a sound effect: we can
+    // always just get the sound and play it.
+
+    // Don't forget a button that lets us enable audio in the browser
+    drawMuteButton({ cx: 15.5, cy: 0.5, width: 1, height: 1, scene: stage.hud });
+
+    // If you tap this actor repeatedly, it will keep playing new copies of the
+    // sound over the old ones:
+    new Actor({
+      appearance: new FilledBox({ width: .75, height: .75, fillColor: "#0000FF" }),
+      rigidBody: new BoxBody({ cx: 1.5, cy: 1.5, width: .75, height: .75 }),
+      gestures: { tap: () => { let x = stage.musicLibrary.getSound("lose_sound.ogg").play(); console.log(x); return true; } }
+    });
+
+    // This actor checks if the sound is playing, and won't start a second copy
+    // while the first is still playing
+    new Actor({
+      appearance: new FilledBox({ width: .75, height: .75, fillColor: "#00FFFF" }),
+      rigidBody: new BoxBody({ cx: 3.5, cy: 1.5, width: .75, height: .75 }),
+      gestures: {
+        tap: () => {
+          let sound = stage.musicLibrary.getSound("lose_sound.ogg");
+          if (!sound.playing()) sound.play();
+          return true;
+        },
+      },
+    });
+
+    // This actor will stop all copies of the sound if you tap it while the
+    // sound is playing
+    new Actor({
+      appearance: new FilledBox({ width: .75, height: .75, fillColor: "#00FF00" }),
+      rigidBody: new BoxBody({ cx: 5.5, cy: 1.5, width: .75, height: .75 }),
+      gestures: {
+        tap: () => {
+          let sound = stage.musicLibrary.getSound("lose_sound.ogg");
+          if (sound.playing()) sound.stop();
+          else sound.play();
+          return true;
+        },
+      },
+    });
+
+    // This actor will pause all copies of the sound if you tap it while the
+    // sound is playing
+    new Actor({
+      appearance: new FilledBox({ width: .75, height: .75, fillColor: "#FF00FF" }),
+      rigidBody: new BoxBody({ cx: 7.5, cy: 1.5, width: .75, height: .75 }),
+      gestures: {
+        tap: () => {
+          let sound = stage.musicLibrary.getSound("lose_sound.ogg");
+          if (sound.playing()) sound.pause();
+          else sound.play();
+          return true;
+        },
+      },
+    });
+
+    // This actor uses its extra to keep track of the ID of the sound, and only
+    // manages its own sound
+    new Actor({
+      appearance: new FilledBox({ width: .75, height: .75, fillColor: "#FFFF00" }),
+      rigidBody: new BoxBody({ cx: 9.5, cy: 1.5, width: .75, height: .75 }),
+      gestures: {
+        tap: (a: Actor) => {
+          let sound = stage.musicLibrary.getSound("lose_sound.ogg");
+          if (sound.playing(a.extra.soundId)) sound.pause(a.extra.soundId);
+          else a.extra.soundId = sound.play();
+          return true;
+        },
+      },
+      extra: { soundId: -1 } // -1 is smaller than any value returned by play()
+    });
+  }
 }
 
 // call the function that kicks off the game
