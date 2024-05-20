63e8b0301a7ce70faef931a743ddaf1003a968c9
Michael Spear <spear@lehigh.edu>
2024-05-20 00:27:30 -0400

    cleaner grid system

--- src/jetlag/Systems/Grid.ts
+++ src/jetlag/Systems/Grid.ts
@@ -18,36 +18,24 @@ export class GridSystem {
    * @param top_left      The x/y coordinates of the top-left corner
    * @param bottom_right  The x/y coordinates of the bottom-right corner
    * @param z             The Z index of the grid
-   * @param halves        True to print half-meter lines
    */
-  public static makeGrid(scene: Scene, top_left: { x: number, y: number }, bottom_right: { x: number, y: number }, z?: ZIndex, halves: boolean = true) {
+  public static makeGrid(scene: Scene, top_left: { x: number, y: number }, bottom_right: { x: number, y: number }, z?: ZIndex) {
     let width = bottom_right.x - top_left.x;
     let height = bottom_right.y - top_left.y;
-    if (z === undefined) z = -2;
-    for (let x = top_left.x; x <= bottom_right.x; x += 1) {
-      new Actor({
-        rigidBody: new BoxBody({ cx: x, cy: top_left.y + height / 2, width: 0.01, height }, { scene }),
-        appearance: new FilledBox({ width: 0.01, height: height, lineWidth: 1, lineColor: "#000000", fillColor: "#00000000", z })
-      });
+    z = z ?? -2;
 
-      if (halves && x < bottom_right.x)
-        new Actor({
-          rigidBody: new BoxBody({ cx: x + .5, cy: top_left.y + height / 2, width: 0.01, height }, { scene }),
-          appearance: new FilledBox({ width: 0.01, height: height, lineWidth: 0.5, lineColor: "#1100dc", fillColor: "#00000000", z })
-        });
+    for (let x = top_left.x + .5; x <= bottom_right.x + 1; x += 2) {
+      new Actor({
+        rigidBody: new BoxBody({ cx: x, cy: top_left.y + height / 2, width: 1, height }, { scene }),
+        appearance: new FilledBox({ width: 1, height, lineWidth: 2, lineColor: "#000000", fillColor: "#00000000", z })
+      });
     }
 
-    for (let y = top_left.y; y <= bottom_right.y; y += 1) {
+    for (let y = top_left.y + .5; y <= bottom_right.y + 1; y += 2) {
       new Actor({
-        rigidBody: new BoxBody({ cx: top_left.x + width / 2, cy: y, width, height: 0.01 }, { scene }),
-        appearance: new FilledBox({ width: width, height: 0.01, lineWidth: 1, lineColor: "#000000", fillColor: "#00000000", z })
+        rigidBody: new BoxBody({ cx: top_left.x + width / 2, cy: y, width, height: 1 }, { scene }),
+        appearance: new FilledBox({ width: width, height: 1, lineWidth: 2, lineColor: "#000000", fillColor: "#00000000", z })
       });
-
-      if (halves && y < bottom_right.y)
-        new Actor({
-          rigidBody: new BoxBody({ cx: top_left.x + width / 2, cy: y + .5, width, height: 0.01 }, { scene }),
-          appearance: new FilledBox({ width: width, height: 0.01, lineWidth: 0.5, lineColor: "#1100dc", fillColor: "#00000000", z })
-        });
     }
   }
 }
