96e2e5f5b16c06bed67f9bd4b466c89cf457ca99
nateippolito <117186920+nateippolito@users.noreply.github.com>
2024-09-16 15:17:33 -0400

    Update game.ts

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,4 +1,4 @@
-import { Actor, BoxBody, CircleBody, FilledBox, FilledCircle, FilledPolygon, GridSystem, Hero, JetLagGameConfig, KeyCodes, ManualMovement, Obstacle, PolygonBody, TextSprite, TimedEvent, initializeAndLaunch, stage } from "../jetlag";
+import { Actor, ActorPoolSystem, BoxBody, CircleBody, FilledBox, FilledCircle, FilledPolygon, Goodie, GridSystem, Hero, JetLagGameConfig, KeyCodes, ManualMovement, Obstacle, PolygonBody, Sensor, Sides, TextSprite, TimedEvent, initializeAndLaunch, stage } from "../jetlag";
 
 /**
  * Screen dimensions and other game configuration, such as the names of all
@@ -12,6 +12,11 @@ class Config implements JetLagGameConfig {
   hitBoxes = true;
 }
 
+class SomethingThatLasts {
+   energyValue = 100;
+   healthValue = 100;
+}
+
 /**
  * This function draws the first scene that shows when the game starts.  In this
  * code, it's an interactive world that cannot be won or lost.  After your game
@@ -21,30 +26,39 @@ class Config implements JetLagGameConfig {
  *
  * @param level Which level of the game should be displayed
  */
-function builder(_level: number) {
+function builder(level: number) {
+  let sessionInfo = stage.storage.getSession("sessionInfo") as SomethingThatLasts;
+  if (!sessionInfo) {
+    stage.storage.setSession("sessionInfo", new SomethingThatLasts());
+    sessionInfo = stage.storage.getSession("sessionInfo") as SomethingThatLasts;
+  }
 
   //Variables
-  let energyValue = 100;
-  let healthValue = 100;
-
   // Draw a grid on the screen, to help us think about the positions of actors.
   // Remember that when `hitBoxes` is true, clicking the screen will show
   // coordinates in the developer console.
-  //GridSystem.makeGrid(stage.world, { x: 0, y: 0 }, { x: 16, y: 9 });
 
 
   //Example text HUD screen at top left to show hunger and stuff
   //Energy
   new Actor({
     rigidBody: new CircleBody({ cx: 1.5, cy: 0.5, radius: .01 }),
-    appearance: new TextSprite({ center: true, face: "Arial", size: 50, color: "#000000" },  () => "Energy: "+energyValue)
+    appearance: new TextSprite({ center: true, face: "Arial", size: 50, color: "#000000" },  () => "Energy: "+sessionInfo.energyValue)
   });
   //Health
   new Actor({
     rigidBody: new CircleBody({ cx: 1.5, cy: 1.5, radius: .01 }),
-    appearance: new TextSprite({ center: true, face: "Arial", size: 50, color: "#000000" },  () => "Health: "+healthValue)
+    appearance: new TextSprite({ center: true, face: "Arial", size: 50, color: "#000000" },  () => "Health: "+sessionInfo.healthValue)
   });
 
+  //MAIN AREA SYSTEM
+  if(level == 1){
+  //Portal to Rathbone
+  new Actor({
+    appearance: new FilledBox({width: 1, height: 1, fillColor: "#000000" }),
+    rigidBody: new BoxBody({ cx: 1, cy: 8, width: 1, height: 1}),
+    role: new Sensor({ heroCollision: () => stage.switchTo(builder, 2) })
+  });}
 
 
 
@@ -57,58 +71,47 @@ function builder(_level: number) {
   });
   
   // Make an obstacle that is a rectangle
-  let box = new Actor({
-    rigidBody: new BoxBody({ cx: 3, cy: 4, width: 1, height: 1 }),
-    appearance: new FilledBox({ width: 1, height: 1, fillColor: "#ff0000", lineWidth: .04, lineColor: "#00ff00" }),
-    role: new Obstacle({
-      heroCollision: (_o: Actor, h: Actor) => {
-        if(energyValue < 100)
-        energyValue+=20
-        if(energyValue>100){
-          energyValue=100
-        }
-      }
-    }),
-    
-  });
+
 
   //Timer
   stage.world.timer.addEvent(new TimedEvent(1, true, () => {
-    if(energyValue>=5){
-    energyValue-=5}
+    if(sessionInfo.energyValue>=5){
+      sessionInfo.energyValue-=5}
 
-    if(energyValue==0){
-      healthValue-=5
+    if(sessionInfo.energyValue==0){
+      sessionInfo.healthValue-=5
     }
   }));
 
-  let box2 = new Actor({
-    rigidBody: new BoxBody({ cx: 8, cy: 7, width: 1, height: 1 }),
-    appearance: new FilledBox({ width: 1, height: 1, fillColor: "#ff0000", lineWidth: .04, lineColor: "#00ff00" }),
-    role: new Obstacle({
-      heroCollision: (_o: Actor, h: Actor) => {
-        energyValue=0
-      }
-    }),
-    
-  });
-  
 
-  // Make hospital
-  let hospital = new Actor({
-    rigidBody: new BoxBody({ cx: 9, cy: 2, width: 1, height: 1 }),
-    appearance: new FilledBox({ width: 1, height: 1, fillColor: " #0f810e ", lineWidth: .04, lineColor: "#00ff00" }),
-    role: new Obstacle({
-      heroCollision: (_o: Actor, h: Actor) => {
-        healthValue+=5
-        energyValue-=10
-        if(healthValue>100){
-          healthValue=100
+ 
+  //RATHBONE LEVEL FOOD SYSTEM
+  if(level == 2)
+    {
+  function createGoodie(){
+    new Actor({
+      appearance: new FilledCircle({ radius: 0.5, fillColor: "#FF0000" }),
+      rigidBody: new CircleBody({ cx: Math.floor((Math.random() * 13)+3), cy: Math.floor((Math.random() * 9)+1), radius: 0.4 }),
+      role: new Goodie({
+        onCollect: () => {
+          sessionInfo.energyValue += 10;
+          if(sessionInfo.energyValue>100){
+            sessionInfo.energyValue = 100;
+          }
+          createGoodie();
+          return true;
         }
-      }
-    }),
-    
-  });
+      })
+    });}
+    createGoodie();
+    //PORTAL BACK TO MAIN AREA
+    new Actor({
+      appearance: new FilledBox({width: 1, height: 1, fillColor: "#000000" }),
+      rigidBody: new BoxBody({ cx: 1, cy: 8, width: 1, height: 1}),
+      role: new Sensor({ heroCollision: () => stage.switchTo(builder, 1) })
+    });
+  }
+  
 
   // Pressing a key will change the hero's velocity
   stage.keyboard.setKeyUpHandler(KeyCodes.KEY_UP, () => (hero.movement as ManualMovement).updateYVelocity(0));
@@ -124,3 +127,4 @@ function builder(_level: number) {
 // call the function that starts running the game in the `game-player` div tag
 // of `index.html`
 initializeAndLaunch("game-player", new Config(), builder);
+
